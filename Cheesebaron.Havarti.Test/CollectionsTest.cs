d16c1d20e7ed581508c92dffbcc1aeda07053428
Tomasz Cielecki <tomasz@ostebaronen.dk>
2012-12-19 23:52:53 +0100

    Added Two PriorityQueue implemenatations, one based on LinkedList another on BinaryTree.

--- Cheesebaron.Havarti.Test/CollectionsTest.cs
+++ Cheesebaron.Havarti.Test/CollectionsTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Cheesebaron.Havarti.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -23,8 +24,6 @@ namespace Cheesebaron.Havarti.Test
 
             foreach (var i in ints)
                 Assert.IsTrue(tree.Contains(i));
-
-            //TODO Make more tests!!
         }
 
         [TestMethod]
@@ -63,4 +62,198 @@ namespace Cheesebaron.Havarti.Test
          * */
         }
     }
+
+    #region TestClass from: http://www.remondo.net/generic-priority-queue-example-csharp/
+    [TestClass]
+    public class PriorityQueueTests
+    {
+        [TestMethod]
+        public void GivenIntegersUnordered()
+        {
+            // arrange
+            var queue = new PriorityQueueLinkedList<int>();
+            var expected = new[] { 3, 21, 100 };
+
+            // act
+            queue.Enqueue(3);
+            queue.Enqueue(100);
+            queue.Enqueue(21);
+
+            Assert.IsTrue(queue.ToArray().SequenceEqual(expected));
+
+            // arrange
+            var queue2 = new PriorityQueueBinaryTree<int>();
+
+            // act
+            queue2.Enqueue(3);
+            queue2.Enqueue(100);
+            queue2.Enqueue(21);
+
+            Assert.IsTrue(queue2.ToArray().SequenceEqual(expected));
+        }
+
+        [TestMethod]
+        public void DequeueTest()
+        {
+            // arrange
+            var queue = new PriorityQueueLinkedList<int>();
+            const int expected = 21;
+
+            // act
+            queue.Enqueue(21);
+            queue.Enqueue(100);
+            queue.Enqueue(3);
+
+            queue.Dequeue();
+            var result = queue.Dequeue();
+
+            Assert.AreEqual(expected, result);
+
+            // arrange
+            var queue2 = new PriorityQueueBinaryTree<int>();
+
+            // act
+            queue2.Enqueue(21);
+            queue2.Enqueue(100);
+            queue2.Enqueue(3);
+
+            queue2.Dequeue();
+            result = queue2.Dequeue();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void IsEmptyTestWithNoItems()
+        {
+            // arrange
+            var queue = new PriorityQueueLinkedList<int>();
+
+            Assert.IsTrue(queue.IsEmpty);
+
+            // arrange
+            var queue2 = new PriorityQueueBinaryTree<int>();
+
+            Assert.IsTrue(queue2.IsEmpty);
+        }
+
+        [TestMethod]
+        public void IsEmptyTestWithItems()
+        {
+            // arrange
+            var queue = new PriorityQueueLinkedList<int>();
+
+            // act
+            queue.Enqueue(1);
+
+            Assert.IsFalse(queue.IsEmpty);
+
+            // arrange
+            var queue2 = new PriorityQueueBinaryTree<int>();
+
+            // act
+            queue2.Enqueue(1);
+
+            Assert.IsFalse(queue2.IsEmpty);
+        }
+
+        [TestMethod]
+        public void PeekTest()
+        {
+            // arrange
+            var queue = new PriorityQueueLinkedList<int>();
+            const int expected = 3;
+            queue.Enqueue(21);
+            queue.Enqueue(100);
+            queue.Enqueue(3);
+
+            // act
+            var result = queue.Peek();
+
+            Assert.AreEqual(expected, result);
+
+            // arrange
+            var queue2 = new PriorityQueueBinaryTree<int>();
+            queue2.Enqueue(21);
+            queue2.Enqueue(100);
+            queue2.Enqueue(3);
+
+            // act
+            result = queue.Peek();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GivenAnArrayOfObjectsUnordered()
+        {
+            // arrange
+            var queue = new PriorityQueueLinkedList<DummyStringComparer2>();
+
+            var t1 = new DummyStringComparer2 { StringOfX = "xxx" };
+            var t2 = new DummyStringComparer2 { StringOfX = "x" };
+            var t3 = new DummyStringComparer2 { StringOfX = "xxxxx" };
+            var t4 = new DummyStringComparer2 { StringOfX = "xx" };
+            var t5 = new DummyStringComparer2 { StringOfX = "xxxx" };
+
+            DummyStringComparer2[] expected = { t2, t4, t1, t5, t3 };
+            queue.Enqueue(t1);
+            queue.Enqueue(t2);
+            queue.Enqueue(t3);
+            queue.Enqueue(t4);
+            queue.Enqueue(t5);
+
+            // act/assert
+            Assert.IsTrue(queue.ToArray().SequenceEqual(expected));
+
+            // arrange
+            var queue2 = new PriorityQueueBinaryTree<DummyStringComparer2>();
+
+            queue2.Enqueue(t1);
+            queue2.Enqueue(t2);
+            queue2.Enqueue(t3);
+            queue2.Enqueue(t4);
+            queue2.Enqueue(t5);
+
+            // act/assert
+            Assert.IsTrue(queue2.ToArray().SequenceEqual(expected));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void DequeueWithNoItemsShouldThrowException()
+        {
+            // arrange
+            var queue = new PriorityQueueLinkedList<int>();
+
+            // act
+            queue.Dequeue();
+
+            // arrange
+            var queue2 = new PriorityQueueBinaryTree<int>();
+
+            // act
+            queue2.Dequeue();
+        }
+    }
+
+    internal class DummyStringComparer2 : IComparable<DummyStringComparer2>
+    {
+        public string StringOfX { get; set; }
+ 
+        #region IComparable<TestCompareClass> Members
+ 
+        public int CompareTo(DummyStringComparer2 other)
+        {
+            // Return values:
+            // <0: This instance smaller than obj.
+            //  0: This instance occurs in the same position as obj.
+            // >0: This instance larger than obj.
+ 
+            return StringOfX.Length.CompareTo(other.StringOfX.Length);
+        }
+ 
+        #endregion
+    }
+    #endregion
 }
---
+++ Cheesebaron.Havarti/Collections/PriorityQueue.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Cheesebaron.Havarti.Collections
+{
+    public interface IPriorityQueue<T> : IEnumerable<T> where T : IComparable<T>
+    {
+        bool IsEmpty { get; }
+        void Enqueue(T item);
+        T Dequeue();
+        T Peek();
+    }
+
+    /// <summary>
+    /// Borrowed from: http://www.remondo.net/generic-priority-queue-example-csharp/
+    /// </summary>
+    public class PriorityQueueLinkedList<T> : IPriorityQueue<T> where T : IComparable<T>
+    {
+        private readonly LinkedList<T> _items;
+
+        public PriorityQueueLinkedList()
+        {
+            _items = new LinkedList<T>();
+        }
+ 
+        public void Enqueue(T item)
+        {
+            if (IsEmpty)
+            {
+                _items.AddFirst(item);
+                return;
+            }
+ 
+            var existingItem = _items.First;
+ 
+            while (existingItem != null && existingItem.Value.CompareTo(item) < 0)
+            {
+                existingItem = existingItem.Next;
+            }
+ 
+            if (existingItem == null)
+                _items.AddLast(item);
+            else
+            {
+                _items.AddBefore(existingItem, item);
+            }
+        }
+ 
+        public T Dequeue()
+        {
+            var value = _items.First.Value;
+            _items.RemoveFirst();
+ 
+            return value;
+        }
+ 
+        public T Peek()
+        {
+            return _items.First.Value;
+        }
+ 
+        public bool IsEmpty
+        {
+            get { return _items.Count == 0; }
+        }
+ 
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+ 
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    /// <summary>
+    /// Based on the idea from: http://www.remondo.net/generic-priority-queue-example-csharp/#comment-4145
+    /// </summary>
+    public class PriorityQueueBinaryTree<T> : IPriorityQueue<T> where T : IComparable<T>
+    {
+        /// <summary>
+        /// Enum determining how to prioritize.
+        /// Either by Lowest value or highest value being highest priority.
+        /// </summary>
+        public enum PriorityMode
+        {
+            Lowest = 0,
+            Highest = 1
+        }
+
+        private readonly BinaryTree<T> _items;
+
+        public PriorityMode Mode { get; set; }
+
+        public PriorityQueueBinaryTree()
+        {
+            _items = new BinaryTree<T>();
+            Mode = PriorityMode.Lowest;
+        }
+
+        /// <summary>
+        /// Enqueue (push) item in Priority Queue.
+        /// </summary>
+        /// <param name="item">IComparable item of type T</param>
+        public void Enqueue(T item)
+        {
+            _items.Add(item);
+        }
+
+        /// <summary>
+        /// Dequeue (pop) item according to PriorityMode.
+        /// </summary>
+        /// <returns>IComparable item of type T</returns>
+        public T Dequeue()
+        {
+            T value;
+            switch (Mode)
+            {
+                case PriorityMode.Highest:
+                    value = _items.RightMost.Data;
+                    break;
+                case PriorityMode.Lowest:
+                default:
+                    value = _items.LeftMost.Data;
+                    break;
+            }
+            _items.Remove(value);
+
+            return value;
+        }
+
+        /// <summary>
+        /// Lets us see which item has the highest priority according to the PriorityMode.
+        /// </summary>
+        /// <returns></returns>
+        public T Peek()
+        {
+            switch (Mode)
+            {
+                case PriorityMode.Highest:
+                    return _items.RightMost.Data;
+                case PriorityMode.Lowest:
+                default:
+                    return _items.LeftMost.Data;
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get { return _items.Count == 0; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
